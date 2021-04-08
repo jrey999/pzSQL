a171ec0ba1e8e27698e192db6499ce6317732948
John R <50639446+jgr1204@users.noreply.github.com>
2021-04-07 22:20:22 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+    name='pzSQL',
+    version='0.1',
+    description='Wrapper around psycopg2',
+    author='John Reynolds',
+    author_email='reynoldsjohngreg@gmail.com',
+    url='https://github.com/jgr1204/pzSQL',
+    packages=['pzsql'],
+     )
