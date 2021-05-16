33a031245f294c042f09321f899081d799e93f93
John <reynoldsjohngreg@gmail.com>
2021-05-15 22:46:41 -0400

    setup package

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
-from distutils.core import setup
+from distutils.core import setup, find_packages
 
 
 setup(
     name='pzSQL',
-    version='0.1',
+    version='0.0.1',
     description='Wrapper around psycopg2',
     author='John Reynolds',
     author_email='reynoldsjohngreg@gmail.com',
     url='https://github.com/jgr1204/pzSQL',
-    packages=['pzsql'],
+    packages=find_packages(),
+    install_requires=['psycopg2'],
+    packages=['pzSQL'],
      )
