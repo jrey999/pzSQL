8507675ccd1d42ce10db1388ada7dd9c85dffd72
jgr1204 <reynoldsjohngreg@gmail.com>
2021-05-25 11:18:59 -0400

    0.0.4

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 
 setup(
     name='pzSQL',
-    version='0.0.3',
+    version='0.0.4',
     description='Wrapper around psycopg2',
     author='John Reynolds',
     author_email='reynoldsjohngreg@gmail.com',
     url='https://github.com/jgr1204/pzSQL',
-    install_requires=['psycopg2'],
+    install_requires=['psycopg2-binary'],
     setup_require=['wheel'],
     keywords=['postgres', 'psycopg2'],
     packages=['pzSQL'],
